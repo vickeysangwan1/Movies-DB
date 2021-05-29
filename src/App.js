2cae805ec7901ecde64aefb4d0628bb08cd445da
vickeysangwan1 <vickeysangwan007@gmail.com>
2021-05-29 18:58:12 +0530

    starter updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import "./styles/App.css";
+
 function App() {
   return (
     <div>
-      <h1>Movies DB</h1>
+      <h1 className="heading">Movies DB</h1>
     </div>
   );
 }
